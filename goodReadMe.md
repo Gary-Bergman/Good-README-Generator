8d06eabd9ad941723061ecf00fbf31f2278b38c0
Gary Bergman <garybergman00@gmail.com>
2020-06-25 17:39:57 -0400

    Started working on basic questions and created function to write a seperate md file

---
+++ goodReadMe.md
@@ -0,0 +1,10 @@
+{
+    "username": "g",
+    "email": "g",
+    "description": "g",
+    "install": "npm i",
+    "usage": "g",
+    "contribution": "g",
+    "test": "npm test",
+    "license": "APM:\t/apm/l/:packageName"
+}
--- index.js
+++ index.js
@@ -1,16 +1,83 @@
+// Global Variables
+var inquirer = require("inquirer");
+var fs = require("fs");
+// var genMarkdown = require("utils/generateMarkdown.js")
+var filename = "goodReadMe.md";
+
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "How would you like to describe your project?",
+        name: "description"
+    },
+    // {
+    //     type: "input",
+    //     message: "What should be included in your table of contents?",
+    //     name: "tableOfContents"
+    // },
+    {
+        type: "input",
+        message: "How should the user run installations?",
+        default: "npm i",
+        name: "install"
+    },
+    {
+        type: "input",
+        message: "How is this project used?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Who contributed to this project?",
+        name: "contribution"
+    },
+    {
+        type: "input",
+        message: "How should the user run tests?",
+        default: "npm test",
+        name: "test"
+    },
+    {
+        type: "list",
+        message: "How is this project licensed?",
+        choices: ["APM:	/apm/l/:packageName", "B", "C", "D"],
+        name: "license"
+    },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+// function writeToFile(fileName, data) {
 
-// function to initialize program
-function init() {
+// }
+// console.log("hello")
+inquirer.prompt(questions).then(function (userAnswers) {
+    fs.writeFile(filename, JSON.stringify(userAnswers, null, 4), function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Success! File was written to goodReadMe.md");
+    })
+});
 
-}
 
-// function call to initialize program
-init();
+
+    // function to initialize program
+    function init() {
+
+    }
+
+    // function call to initialize program
+    init();
+
+
